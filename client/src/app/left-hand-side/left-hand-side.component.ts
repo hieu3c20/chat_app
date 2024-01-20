2562961fd8458dc84a23e39de5e3867d2b0682b9
hieu3c20 <hovanhieu2012sch@gmail.com>
2024-01-20 18:45:12 +0700

    [FE] adjust body interface

--- client/src/app/left-hand-side/left-hand-side.component.ts
+++ client/src/app/left-hand-side/left-hand-side.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-left-hand-side',
